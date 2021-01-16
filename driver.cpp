d7ed7e1e965994d18074b53a5bbbaae04ef9af12
Keith Lancaster <klancaster1957@acm.org>
2021-01-16 13:36:41 -0800

    initial commit

---
+++ driver.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+double calculateCost(int, double); 
+
+double calculateDiscountedCost(double discount, double baseCost);
+
+int main(int argc, char *argv[])
+{
+  int quantity = 3;
+  double unitCost = 3.50;
+  double cost = calculateCost(quantity,  unitCost);
+  cout << "The cost is " << cost << endl;
+  return 0;
+}
+
+double calculateCost(int quantity, double unitCost){
+  return quantity * unitCost;
+}
+
+double calculateDiscountedCost(double discount, double baseCost){
+    return baseCost - discount * baseCost; // or baseCost(1.0 - discount)
+}
+
