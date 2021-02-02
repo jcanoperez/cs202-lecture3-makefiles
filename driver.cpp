3504b5ff4f00ebec2259b70182c4466a78961908
Keith Lancaster <keithlancaster@unr.edu>
2021-02-02 10:15:56 -0800

    base for refactor demo

--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,17 @@
 #include "calculator.h"
 #include "printer.h"
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
+
+double calculateCost(int quantity, double unitCost);
+
+double calculateDiscountedCost(double discount, double baseCost);
+
+void printErrorMessage(string message);
+
+void printFormattedMessage(string message, double value);
+
 
 
 int main(int argc, char *argv[])
@@ -22,4 +31,25 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+double calculateCost(int quantity, double unitCost){
+  if(quantity < 0){
+    printErrorMessage("Cannot calculate cost if quantity < 0");
+    return -1;
+  }
+  return quantity * unitCost;
+}
+
+double calculateDiscountedCost(double discount, double baseCost){
+  return baseCost - 0.01 * discount * baseCost; // or baseCost(1.0 - discount)
+}
+
+void printErrorMessage(string message){
+  cerr << "---------------" << endl;
+  cerr << message << endl;
+  cerr << "---------------" << endl;
+}
+
+void printFormattedMessage(string message, double value){
+  cout << message << " : " << setprecision(3) << value << endl;
+}
 
